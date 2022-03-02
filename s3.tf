6b6f1534b4461147e34145c19911ced9f83eddcc
KranthiDevtest <98391432+KranthiDevtest@users.noreply.github.com>
2022-03-02 16:40:11 +0530

    changed

--- s3.tf
+++ s3.tf
@@ -1,12 +1,10 @@
-resource "aws_s3_bucket" "mybucket" {
-  bucket_prefix = "kranhi-s3-bucket"
-  acl    = "private"
-  versioning { enabled = true }
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
+resource "aws_s3_bucket" "my-s3-bucket" {
+  bucket_prefix = var.bucket_prefix
+  acl = var.acl
+  
+   versioning {
+    enabled = var.versioning
   }
+  
+  tags = var.tags
 }
